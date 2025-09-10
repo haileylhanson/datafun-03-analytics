2816b3d0fea9f5c5cce6a59e44fdb18236979039
Hailey Hanson <haileylhanson@gmail.com>
2025-09-10 16:03:30 -0700

    copied & successfully ran all example get & process .py files

--- haileyhanson_project_mainscript.py
+++ haileyhanson_project_mainscript.py
@@ -1,2 +1,19 @@
 import requests
 
+
+#####################################
+#Functions
+#####################################
+
+
+def main() -> None:
+    logger.info("Starting main execution of mainscript")
+    url = https://www.kaggle.com/datasets/ma7555/cat-breeds-dataset/cats.csv
+
+    
+#####################################
+# Main Execution
+#####################################
+
+if __name__ == "__main__":
+    main()
